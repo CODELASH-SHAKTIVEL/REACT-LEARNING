188d6482e87c9d1dc28bb5a3e743d0ae34547dbd
JAVASCRIPT-LEARNING <shaktivel.elangovan2004@gmail.com>
2023-09-06 10:26:37 +0530

    Add new features for React component

---
+++ src/Components/Videolist.js
@@ -0,0 +1,32 @@
+import Video from "./video";
+import PlayButton from "./PlayButton";
+
+function Videolisted({videos,deleteVideo,editVideo}){
+
+    return(
+        <>
+        {videos.map((video) => (
+            <Video
+              key={video.id}
+              title={video.title}
+              views={video.views}
+              time={video.time}
+              channel={video.channel}
+              verified={video.verified}
+              id={video.id}
+              deleteVideo={deleteVideo}
+              editVideo={editVideo}
+            >
+              <PlayButton
+                onPlay={() => console.log('Playing..',video.title)}
+                onPause={() => console.log('Paused..',video.title)}
+              >
+                {video.title}
+              </PlayButton>
+            </Video>
+          ))}
+          </>
+    )
+}
+
+export default Videolisted;
